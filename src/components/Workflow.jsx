89eb2884aee933e42c11960c11c38ff547eb8a82
om rakholiya <omrakholiya3126@gmail.com>
2025-08-28 09:33:06 +0530

    some changes

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -7,7 +7,7 @@ const Workflow = () => {
     <div className="mt-20">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
         Accelerate your{" "}
-        <span className=" bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
           coding workflow.
         </span>
       </h2>
