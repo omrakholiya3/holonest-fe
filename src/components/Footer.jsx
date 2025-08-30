99e92c2f354f0d135585434b353922842f626788
om rakholiya <omrakholiya3126@gmail.com>
2025-08-30 11:09:29 +0530

    some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-          <h3 className="text-md font-semibold mb-4">Community</h3>
+          <h3 className=" text-md font-semibold mb-4">Community</h3>
           <ul className="space-y-2">
             {communityLinks.map((link, index) => (
               <li key={index}>
